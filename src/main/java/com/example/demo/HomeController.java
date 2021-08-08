11ece54144cfd7f6d0de01e0c2d78b02474a57b8
Trisha Chakraborty <trisha123chak@gmail.com>
2021-08-08 13:33:07 -0600

    Returning jsp file in Springboot

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,13 +2,21 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 	
-	@RequestMapping
-	public void home() {
+	@RequestMapping("home")
+	//@ResponseBody
+	/*
+	 * Use ResponseBody to display return anything written inside the controller. As
+	 * jsp not supported by Springboot, to return a jsp page, need to add mvn
+	 * dependency "tomcat-jasper" in pom.xml file.
+	 */	
+	public String home() {
 		System.out.println("Reached home controller");
+		return "home.jsp";
 	}
 
 }
--- src/main/webapp/home.jsp
+++ src/main/webapp/home.jsp
@@ -4,9 +4,9 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>Home Page</title>
 </head>
 <body>
-
+I am inside home page body
 </body>
 </html>
